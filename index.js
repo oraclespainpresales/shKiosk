4791b5ba09b3bb3de59015fb6ffc7c95b6a1ea7b
Carlos Casares <ccasares@gmail.com>
2017-11-15 11:49:42 +0100

    fix

--- index.js
+++ index.js
@@ -46,6 +46,7 @@ io.on('connection', function(socket) {
   });
 
   socket.on('reset-stream', function() {
+    console.log("Reset streaming request...");
     stopStreaming();
     startStreaming(io);
   });
